0f57b60be02cf0d195f1138ff3faa436dff115e1
ndarmage <daniele.tomatis@outlook.com>
2017-06-02 00:34:50 +0200

    fix bad conf.py; numpy not needed in MOCK_MODULES

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['gfortran', 'numpy']
+MOCK_MODULES = ['gfortran']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
